da70e78520e0bc51d86362562fc23c1c4e0bae56
Darvin Segura <dseguradiaz@quantiva.co>
2023-11-14 19:17:55 -0500

    Initial Commit

---
+++ weather-forecast-test/weather_forecast_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_forecast_testApp.swift
+//  weather-forecast-test
+//
+//  Created by Darvin Segura diaz on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_forecast_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
